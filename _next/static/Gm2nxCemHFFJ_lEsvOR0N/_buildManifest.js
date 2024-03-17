d3950190a82cb8a599d8bf4d056ad119aa31fa76
Giovambattista Fazioli <giovambattista.fazioli@gmail.com>
2024-03-17 12:17:01 +0100

    Updates

--- _next/static/Gm2nxCemHFFJ_lEsvOR0N/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/7249-dba701961e47513a.js","static/css/411d08967b80a6bc.css","static/chunks/pages/index-f8d1ff2b67b0c447.js"],"/_error":["static/chunks/pages/_error-78f2934ae700e086.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
